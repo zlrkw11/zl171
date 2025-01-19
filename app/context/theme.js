8bd7c1ed393f60eb3edf11a4148b6354500cf5f6
ZL <ray040406@gmail.com>
2025-01-19 17:28:06 +1300

    whenever theme changes, we run the useEffect

--- app/context/theme.js
+++ app/context/theme.js
@@ -3,13 +3,20 @@ import { createContext, useContext, useState, useEffect } from "react";
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("dark");
+  const [theme, setTheme] = useState();
+  useEffect(() => {
+    document.documentElement.setAttribute("data-bs-theme", theme);
+  }, [theme]);
   const toggleTheme = () => {
     return setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
   };
   return (
-    <ThemeContext.Provider value={{ theme, setTheme }}>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
       {children}
     </ThemeContext.Provider>
   );
+};
+
+export const useTheme = () => {
+  useContext(ThemeContext);
 };
