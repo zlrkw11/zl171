e76e7040d3417ffc3527bf314c6e2235690858f8
ZL <ray040406@gmail.com>
2025-01-22 00:47:53 +1300

    getDefaultTheme function

--- app/context/theme.js
+++ app/context/theme.js
@@ -2,11 +2,19 @@
 import { createContext, useContext, useState, useEffect } from "react";
 const ThemeContext = createContext();
 
+const getDefaultTheme = () => {
+  if (typeof window !== "undefined") {
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme || "dark";
+  }
+  return "dark";
+};
+
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState();
+  const [theme, setTheme] = useState(getDefaultTheme());
   useEffect(() => {
     document.documentElement.setAttribute("data-bs-theme", theme);
-    localStorage.setItem("theme", theme);
+    localStorage.setItem("theme", theme); // save theme to local storage
   }, [theme]);
   const toggleTheme = () => {
     return setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
